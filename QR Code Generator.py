73e60339b844639321d60c26cf3223467fb61a94
Aloka Pradhan <117985601+Alok774@users.noreply.github.com>
2024-03-17 22:39:02 -0700

    Add files via upload

---
+++ QR Code Generator.py
@@ -0,0 +1,3 @@
+import qrcode as qr
+img=qr.make('Name:Aloka Peadhann\nWork:Software Engineer\nClick My youtube channel-https://www.youtube.com/channel/UCRw43us8OQXN28qzoELtg6w\nThamk you using my Qr.')
+img.save("Alok_youtube.png")
